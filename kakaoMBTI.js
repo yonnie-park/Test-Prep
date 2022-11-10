6c30dfe79c8a7fe0d066e131e4309616b157572c
yonnie-park <juon0805@gmail.com>
2022-11-10 15:00:12 +0900

    commit

--- kakaoMBTI.js
+++ kakaoMBTI.js
@@ -11,17 +11,13 @@ function solution(survey, choices) {
         N: 0,
     };
 
-    
-    let ans = "";
+    let result = ""; //"TCMA" string
     
    
     for(let i = 0; i < survey.length; i++){
-       
         let [no, yes] = survey[i].split("");
-        
-        // survey[i]에 대한 유저의 선택값
         let grade = choices[i]
-        
+
         if(grade === 4){
            
             continue;
@@ -68,3 +64,30 @@ function solution(survey, choices) {
     
     return ans;
 }
+
+/*
+
+****다른분 문제풀이****
+
+function solution(survey, choices) {
+    const MBTI = {};
+    const types = ["RT","CF","JM","AN"];
+
+    // 일일이 지정해서 객체를 만든게 아니라 자동으로 객체를 만드셨다.
+    types.forEach((type) =>
+        type.split('').forEach((char) => MBTI[char] = 0)
+    )
+
+    // 
+    choices.forEach((choice, index) => {
+        // 이렇게 해도 하나의 문자열이 나눠지는구나!
+        const [disagree, agree] = survey[index];
+
+        // chice의 값에 따라 성향을 나눠, 위에서 만든 MBTI 객체의 key값에 넣어준다
+        // 점수 환산 로직에 맞춰서 산술한 값을 누적합 한다.
+        MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
+    });
+
+    // map에서도 이런 방식으로 하나의 문자열을 a, b로 나눌 수 있구나..!
+    return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join("");
+}*/
