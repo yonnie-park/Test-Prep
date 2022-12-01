808880ed1aaaeee9cace05494160656ebecec0a0
yonnie-park <juon0805@gmail.com>
2022-12-01 13:30:37 +0900

    commit

---
+++ programmers/kakao_problems/kakaoArchery.js
@@ -0,0 +1,9 @@
+function solution(n, info) {
+    let max=0, ryan=Array(11).fill(0)
+    
+    if(max<=0) return -1 //라이언이 못이김
+    return ryan;
+}
+
+console.log(solution(5,	[2,1,1,1,0,0,0,0,0,0,0])) //[0,2,2,0,1,0,0,0,0,0,0]
+
